37c69903bbdcf8290283bbdd6817dd127d18d895
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-07 16:04:40 +0000

    Updates Violin class to implement ISell interface

--- src/main/java/instruments/Violin.java
+++ src/main/java/instruments/Violin.java
@@ -4,8 +4,8 @@ public class Violin extends Instrument {
 
     private int numberOfStrings;
 
-    public Violin(String colour, String material, String maker, int numberOfStrings) {
-        super(colour, material, maker);
+    public Violin(String colour, String material, String maker, int numberOfStrings, double boughtPrice, double sellPrice) {
+        super(colour, material, maker, boughtPrice, sellPrice);
         this.numberOfStrings = numberOfStrings;
 
         setInstrumentType(InstrumentType.STRING);
@@ -17,5 +17,10 @@ public class Violin extends Instrument {
 
     public String playInstrument() {
         return "Pluck, pluck, pluck!";
+    }
+
+    public String getDescription() {
+        String description = String.format("%s violin, Colour: %s, Material: %s, Price: £%.2f", super.getMaker(), super.getColour(), super.getMaterial(), super.getSellPrice());
+        return description;
     }
 }
