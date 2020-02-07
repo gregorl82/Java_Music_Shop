034415f4a440654f2d74238b1e58bedccef8f9fa
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-07 13:54:50 +0000

    Adds tests for Metronome class

---
+++ src/test/java/MetronomeTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class MetronomeTest {
+
+    private Metronome metronome;
+
+    @Before
+    public void before(){
+        metronome = new Metronome("2180 Pyramid Metronome", "Wittner", MetronomeType.TRADITIONAL);
+    }
+
+    @Test
+    public void hasAModelName(){
+        assertEquals("2180 Pyramid Metronome", metronome.getName());
+    }
+
+    @Test
+    public void hasAMaker(){
+        assertEquals("Wittner", metronome.getMaker());
+    }
+
+    @Test
+    public void hasAMetronomeType(){
+        assertEquals("Traditional", metronome.getMetronomeType());
+    }
+}
