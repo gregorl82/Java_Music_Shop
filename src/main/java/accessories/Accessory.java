a23153b527b9a8ca4d111004241ece7b0707bd44
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-07 14:46:11 +0000

    Updates Metronome class to implement ISell interface

--- src/main/java/accessories/Accessory.java
+++ src/main/java/accessories/Accessory.java
@@ -1,13 +1,19 @@
 package accessories;
 
-public abstract class Accessory {
+import interfaces.ISell;
+
+public abstract class Accessory implements ISell {
 
     private String name;
     private String maker;
+    private double boughtPrice;
+    private double sellPrice;
 
-    public Accessory(String name, String maker) {
+    public Accessory(String name, String maker, double boughtPrice, double sellPrice) {
         this.name = name;
         this.maker = maker;
+        this.boughtPrice = boughtPrice;
+        this.sellPrice = sellPrice;
     }
 
     public String getName() {
@@ -16,5 +22,21 @@ public abstract class Accessory {
 
     public String getMaker() {
         return maker;
+    }
+
+    public double getBoughtPrice() {
+        return boughtPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public double calculateMarkup(){
+        return this.sellPrice - this.boughtPrice;
     }
 }
--- src/main/java/accessories/Metronome.java
+++ src/main/java/accessories/Metronome.java
@@ -4,12 +4,19 @@ public class Metronome extends Accessory {
 
     private MetronomeType metronomeType;
 
-    public Metronome(String name, String maker, MetronomeType metronomeType) {
-        super(name, maker);
+    public Metronome(String name, String maker, MetronomeType metronomeType, double boughtPrice, double sellPrice) {
+        super(name, maker, boughtPrice, sellPrice);
         this.metronomeType = metronomeType;
     }
 
     public String getMetronomeType() {
         return metronomeType.getType();
     }
+
+    @Override
+    public String getDescription() {
+        String description = String.format("Name: %s, Maker: %s, Type: %s, Price: £%.2f", super.getName(), super.getMaker(), getMetronomeType(), super.getSellPrice());
+        return description;
+    }
+
 }
