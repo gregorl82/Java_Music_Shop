443dfa94fa91c84f6e73d8ffc6bf0c568ccacdaa
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-07 15:51:44 +0000

    Updates Clarinet test for implemention of ISell interface

--- src/test/java/ClarinetTest.java
+++ src/test/java/ClarinetTest.java
@@ -10,7 +10,7 @@ public class ClarinetTest {
 
     @Before
     public void before(){
-        clarinet = new Clarinet("Black", "Grenadilla wood", "Yamaha");
+        clarinet = new Clarinet("Black", "Grenadilla wood", "Yamaha", 455.0, 560.0);
     }
 
     @Test
@@ -36,6 +36,33 @@ public class ClarinetTest {
     @Test
     public void canPlayInstrument(){
         assertEquals("Toot, toot, toot!", clarinet.playInstrument());
+    }
+
+    @Test
+    public void hasABoughtPrice(){
+        assertEquals(455.0, clarinet.getBoughtPrice(), 0.01);
+    }
+
+    @Test
+    public void hasASellPrice(){
+        assertEquals(560.0, clarinet.getSellPrice(), 0.01);
+    }
+
+    @Test
+    public void canSetSellPrice(){
+        clarinet.setSellPrice(510.0);
+        assertEquals(510.0, clarinet.getSellPrice(), 0.01);
+    }
+
+    @Test
+    public void canCalculateMarkup(){
+        assertEquals(105.0, clarinet.calculateMarkup(), 0.01);
+    }
+
+    @Test
+    public void canGetDescription(){
+        String description = "Yamaha clarinet, Colour: Black, Material: Grenadilla wood, Price: £560.00";
+        assertEquals(description, clarinet.getDescription());
     }
 
 }
