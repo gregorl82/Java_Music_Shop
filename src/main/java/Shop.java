c966a71b7f9a72c6b9353d55449861f433a1878c
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-07 16:19:18 +0000

    Adds Shop class with stock of ISell objects, can add and remove

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,26 @@
+import instruments.Piano;
+import interfaces.ISell;
+import sheetmusic.SheetMusic;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private ArrayList<ISell> stock;
+
+    public Shop(){
+        this.stock = new ArrayList<>();
+    }
+
+    public int countStock() {
+        return this.stock.size();
+    }
+
+    public void addStock(ISell item) {
+        this.stock.add(item);
+    }
+
+    public void removeStock(ISell item) {
+        this.stock.remove(item);
+    }
 }
