3874873eff848c5649155106eb3ec208f2ac8550
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-07 15:34:36 +0000

    Updates PianoTest class to test implemention of ISell interface

--- src/test/java/PianoTest.java
+++ src/test/java/PianoTest.java
@@ -10,7 +10,7 @@ public class PianoTest {
 
     @Before
     public void before(){
-        piano = new Piano("Mahogany", "Hardwood", "Steinway", "Grand", 72);
+        piano = new Piano("Mahogany", "Hardwood", "Steinway", "Grand", 72, 18500.0, 23950.0);
     }
 
     @Test
@@ -44,7 +44,34 @@ public class PianoTest {
     }
 
     @Test
+    public void hasABoughtPrice(){
+        assertEquals(18500.0, piano.getBoughtPrice(), 0.01);
+    }
+
+    @Test
+    public void hasASellPrice(){
+        assertEquals(23950.0, piano.getSellPrice(), 0.01);
+    }
+
+    @Test
+    public void canSetSellPrice(){
+        piano.setSellPrice(22500.0);
+        assertEquals(22500.0, piano.getSellPrice(), 0.01);
+    }
+
+    @Test
+    public void canCalculateMarkup(){
+        assertEquals(5450.0, piano.calculateMarkup(), 0.01);
+    }
+
+    @Test
     public void canPlayInstrument(){
         assertEquals("Plink, plink, plink!", piano.playInstrument());
+    }
+
+    @Test
+    public void canGetDescription(){
+        String expected = "Steinway Grand piano, Colour: Mahogany, Material: Hardwood, Price: £23950.00";
+        assertEquals(expected, piano.getDescription());
     }
 }
