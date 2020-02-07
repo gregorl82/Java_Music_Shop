1230ea2a7a3f36d70be45659df29531f5f177a9a
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-07 15:05:16 +0000

    Updates SheetMusic class to implement ISell interface

--- src/main/java/sheetmusic/SheetMusic.java
+++ src/main/java/sheetmusic/SheetMusic.java
@@ -1,15 +1,21 @@
 package sheetmusic;
 
-public class SheetMusic {
+import interfaces.ISell;
+
+public class SheetMusic implements ISell {
 
     private String title;
     private String composer;
     private String forInstrument;
+    private double boughtPrice;
+    private double sellPrice;
 
-    public SheetMusic(String title, String composer, String forInstrument) {
+    public SheetMusic(String title, String composer, String forInstrument, double boughtPrice, double sellPrice) {
         this.title = title;
         this.composer = composer;
         this.forInstrument = forInstrument;
+        this.boughtPrice = boughtPrice;
+        this.sellPrice = sellPrice;
     }
 
     public String getTitle() {
@@ -22,5 +28,26 @@ public class SheetMusic {
 
     public String getForInstrument() {
         return forInstrument;
+    }
+
+    public double getBoughtPrice() {
+        return boughtPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public String getDescription() {
+        String description = String.format("Title: %s, Composer: %s, For: %s, Price: £%.2f", this.title, this.composer, this.forInstrument, this.sellPrice);
+        return description;
+    }
+
+    public double calculateMarkup() {
+        return this.sellPrice - this.boughtPrice;
     }
 }
--- src/test/java/SheetMusicTest.java
+++ src/test/java/SheetMusicTest.java
@@ -10,7 +10,7 @@ public class SheetMusicTest {
 
     @Before
     public void before(){
-        sheetMusic = new SheetMusic("Eine kleine Nachtmusik", "Wolfgang Amadeus Mozart", "String ensemble");
+        sheetMusic = new SheetMusic("Eine kleine Nachtmusik", "Wolfgang Amadeus Mozart", "String ensemble", 3.25, 7.5);
     }
 
     @Test
@@ -26,5 +26,32 @@ public class SheetMusicTest {
     @Test
     public void hasAnInstrument(){
         assertEquals("String ensemble", sheetMusic.getForInstrument());
+    }
+
+    @Test
+    public void hasABoughtPrice(){
+        assertEquals(3.25, sheetMusic.getBoughtPrice(), 0.01);
+    }
+
+    @Test
+    public void hasASellPrice(){
+        assertEquals(7.50, sheetMusic.getSellPrice(), 0.01);
+    }
+
+    @Test
+    public void canSetSellPrice(){
+        sheetMusic.setSellPrice(6.95);
+        assertEquals(6.95, sheetMusic.getSellPrice(), 0.01);
+    }
+
+    @Test
+    public void canCalculateMarkup(){
+        assertEquals(4.25, sheetMusic.calculateMarkup(), 0.01);
+    }
+
+    @Test
+    public void canGetDescription(){
+        String expected = "Title: Eine kleine Nachtmusik, Composer: Wolfgang Amadeus Mozart, For: String ensemble, Price: £7.50";
+        assertEquals(expected, sheetMusic.getDescription());
     }
 }
